1e7d39c90788c8a4d87f64a7c9edf321e50e079e
s309876 <s309876@55PCH017>
2019-12-09 19:47:12 +0000

    fix class user

--- user.cpp
+++ user.cpp
@@ -1,19 +1,4 @@
 class user {
-public:
-	double userID;
-	double resource;
-	double timeCost;
-	double priceCost;
-	double jobNumber;
-	double nodeNumber;
-	double nodeHours;
-
-
-private:
-	void setUserID() {}
-	void getUserID() {}
-	void setResource() {}
-
 
 
 };
--- user.h
+++ user.h
@@ -1,4 +1,28 @@
 class user {
+public:
+	int userID;
+	double resource;
+	double timeCost;
+	double priceCost;
+	double jobNumber;
+	double nodeNumber;
+	double nodeHours;
 
+
+private:
+	void setUserID() {}
+	void getUserID() {}
+	void setResource() {}
+	void getResource() {}
+	void setTimeCost() {}
+	void getTimeCost() {}
+	void setPriceCost() {}
+	void getPriceCost() {}
+	void setJobNumber() {}
+	void getJobNumber() {}
+	void setNodeNumber() {}
+	void getNodeNumber() {}
+	void setNodeHours() {}
+	void getNodeHours() {}
 
 };
