13e257979a4e72be2d3bab9d37166e25628396d5
s309876 <s309876@55PCH017>
2019-12-10 15:22:05 +0000

    add resource class and input method

---
+++ resource.cpp
@@ -0,0 +1,31 @@
+#include"resource.h"
+
+void Resource::setUserType(char userType)
+{
+	this->userType = userType;
+}
+
+char Resource::getUserType()
+{
+	return this->userType;
+}
+
+void Resource::setGroupResource(double groupResource)
+{
+	this->groupResource = groupResource;
+}
+
+double Resource::getGroupResource()
+{
+	return this->groupResource;
+}
+
+void Resource::setGroupType(char groupType)
+{
+	this->groupType = groupType;
+}
+
+char Resource::getGroupType()
+{
+	return this->groupType;
+}
---
+++ resource.h
@@ -0,0 +1,15 @@
+class Resource{
+private:
+	char userType;
+	double groupResource;
+	char groupType;
+public:
+	void setUserType(char userType);
+	char getUserType();
+	void setGroupResource(double groupResource);
+	double getGroupResource();
+	void setGroupType(char groupType);
+	char getGroupType();
+
+
+}
