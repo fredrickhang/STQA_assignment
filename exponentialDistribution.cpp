a5116389573a50f9c833975f6213ead63192f058
s309876 <s309876@55PCH017>
2019-12-11 21:17:48 +0000

    fix

--- exponentialDistribution.cpp
+++ exponentialDistribution.cpp
@@ -1,12 +1,12 @@
 #include "exponentialDistribution.h"
 #include <random>
 #include <iostream>
-
+using namespace std;
 void exponentialDistribution(double randomWaitTimeList[500]) {
 	
-	std::random_device rd;
-	std::exponential_distribution<>rng(1 / 0.2);
-	std::mt19937 rnd_gen(rd());
+	random_device rd;
+	exponential_distribution<>rng(1 / 0.2);
+	mt19937 rnd_gen(rd());
 
 	for (int k = 0; k < 500; k++) {
 		randomWaitTimeList[k] = rng(rnd_gen);
